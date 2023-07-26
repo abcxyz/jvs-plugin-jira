f7232e84ee9c9ee77aa68ae02d73adda03f1fbfb
Dandan Shen <dandans@google.com>
2023-07-26 22:51:12 +0000

    add license, resolve comments

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -1,3 +1,17 @@
+# Copyright 2023 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 resource "google_project_service" "services" {
   project = var.project_id
 
@@ -44,6 +58,18 @@ resource "google_secret_manager_secret_iam_member" "jira_api_token_accessor" {
   secret_id = google_secret_manager_secret.jira_api_token.secret_id
 
   role = "roles/secretmanager.secretAccessor"
+
+  member = each.value
+}
+
+resource "google_secret_manager_secret_iam_member" "jira_api_token_admin" {
+  for_each = toset(var.jira_api_token_admin)
+
+  project = var.project_id
+
+  secret_id = google_secret_manager_secret.jira_api_token.secret_id
+
+  role = "roles/secretmanager.admin"
 
   member = each.value
 }
