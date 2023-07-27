03acc9bf57a1abfa8f7c927e0af90f1dc118fc0a
Dandan Shen <118029912+dandans-verily@users.noreply.github.com>
2023-07-27 16:18:15 -0700

    add terraform modules to actuate secret manager to hold api token (#25)
    Closes: #6 
    Linkto: #7
    
    ---------
    
    Signed-off-by: Dandan Shen <118029912+dandans-verily@users.noreply.github.com>
    Co-authored-by: Chen Shou <cshou@google.com>

---
+++ terraform/modules/secret_manager/main.tf
@@ -0,0 +1,78 @@
+# Copyright 2023 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_project_service" "services" {
+  project = var.project_id
+
+  service                    = "secretmanager.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
+
+# Secret Manager secrets for jira plugin to use.
+resource "google_secret_manager_secret" "jira_api_token" {
+  project = var.project_id
+
+  secret_id = var.secret_id
+  labels    = var.labels
+  replication {
+    automatic = true
+  }
+
+  depends_on = [
+    google_project_service.services
+  ]
+}
+
+resource "google_secret_manager_secret_version" "jira_api_token_version" {
+  secret = google_secret_manager_secret.jira_api_token.id
+
+  # default value used for initial revision to allow cloud run to map the secret
+  # to manage this value and versions, use the google cloud web application
+  secret_data = "DEFAULT_VALUE"
+
+  lifecycle {
+    ignore_changes = [
+      enabled,
+
+      # Ignore secret data so Terraform won't reset the secret.
+      # Operator will need to put the JIRA credential into the secret manually.
+      secret_data,
+    ]
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "jira_api_token_accessor" {
+  for_each = toset(var.jira_api_token_accessors)
+
+  project = var.project_id
+
+  secret_id = google_secret_manager_secret.jira_api_token.secret_id
+
+  role = "roles/secretmanager.secretAccessor"
+
+  member = each.value
+}
+
+resource "google_secret_manager_secret_iam_member" "jira_api_token_admin" {
+  for_each = toset(var.jira_api_token_admin)
+
+  project = var.project_id
+
+  secret_id = google_secret_manager_secret.jira_api_token.secret_id
+
+  role = "roles/secretmanager.admin"
+
+  member = each.value
+}
---
+++ terraform/modules/secret_manager/variables.tf
@@ -0,0 +1,54 @@
+# Copyright 2023 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project_id" {
+  description = "GCP project ID where the secrets belongs."
+  type        = string
+}
+
+variable "secret_id" {
+  description = <<EOT
+    ID of the secret. This must be unique within the project. This is the secret
+    name.
+  EOT
+  type        = string
+}
+
+variable "labels" {
+  description = "The labels assigned to this Secret."
+  type        = map(string)
+  default     = {}
+}
+
+variable "jira_api_token_accessors" {
+  description = <<EOT
+    List of IAM members with the secret manager secret accessor role. These users will be
+    given the role "roles/secretmanager.secretAccessor". Please reference
+    https://cloud.google.com/iam/docs/overview#cloud-iam-policy
+    for the format for member identifiers.
+  EOT
+  type        = list(string)
+  default     = []
+}
+
+variable "jira_api_token_admin" {
+  description = <<EOT
+    List of IAM members with the secret manager admin role. These users will be
+    given the role "roles/secretmanager.admin". Please reference
+    https://cloud.google.com/iam/docs/overview#cloud-iam-policy
+    for the format for member identifiers.
+  EOT
+  type        = list(string)
+  default     = []
+}
