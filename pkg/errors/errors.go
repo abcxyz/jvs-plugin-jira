61ce3e7221fd5c4c7c01ae2cdded641ceab8d239
jiahaluo <116128330+jiahaluo@users.noreply.github.com>
2023-08-30 13:58:56 -0700

    Update pkg/errors/errors.go
    Co-authored-by: Chen Shou <cshou@google.com>
    Signed-off-by: jiahaluo <116128330+jiahaluo@users.noreply.github.com>

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -15,14 +15,6 @@
 // Error is a concrete error implementation.
 package errors
 
-type Error string
-
-// Error satisfies the error interface.
-func (e Error) Error() string {
-	return string(e)
-}
-
-const (
-	ErrInvalidJustification = Error("invalid justification")
-	ErrInternal             = Error("internal error, unable to perform jira validation")
+var (
+	ErrInvalidJustification = fmt.Errorf("invalid justification")
 )
